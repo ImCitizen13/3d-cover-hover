40ee0f383eeafee4cb230f4ade4b6794e3d9c212
ImCitizen13 <meltohamy@protonmail.com>
2023-09-25 13:35:07 +0400

    added title

--- src/App.tsx
+++ src/App.tsx
@@ -29,6 +29,14 @@ function App() {
       fgImgSrc:
         "https://github.com/ImCitizen13/3d-cover-hover/blob/main/src/assets/tyrion_fg.png?raw=true",
     },
+    // {
+    //   bgImgSrc:
+    //   "https://assets.codepen.io/308367/yoda-on-dagobah.jpg",
+    //   logoImgSrc:
+    //   "https://assets.codepen.io/308367/yoda-logo.webp",
+    //   fgImgSrc:
+    //   "https://assets.codepen.io/308367/yoda-front.png",
+    // },
     {
       bgImgSrc:
         "https://github.com/ImCitizen13/3d-cover-hover/blob/main/src/assets/op_bg.jpg?raw=true",
@@ -39,14 +47,22 @@ function App() {
     },
   ];
   return (
-    <div className="hover-cards">
-      {hoverCards.map((hCard) => {
-        return <HoverCardView
-          bgImgSrc={hCard.bgImgSrc}
-          logoImgSrc={hCard.logoImgSrc}
-          fgImgSrc={hCard.fgImgSrc}
-        ></HoverCardView>;
-      })}
+    <div>
+      <div className="title-header">
+        <h1 className="rs-shop-header">3D Hover Cards. </h1>
+        <div className="rs-shop-subheader">Take a look.</div>
+      </div>
+      <div className="hover-cards">
+        {hoverCards.map((hCard) => {
+          return (
+            <HoverCardView
+              bgImgSrc={hCard.bgImgSrc}
+              logoImgSrc={hCard.logoImgSrc}
+              fgImgSrc={hCard.fgImgSrc}
+            ></HoverCardView>
+          );
+        })}
+      </div>
     </div>
   );
 }
