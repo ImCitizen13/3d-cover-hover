8845d293aafc3a86ff5e482f243131aed8d4e46b
ImCitizen13 <meltohamy@protonmail.com>
2023-09-25 12:26:26 +0400

    Added the effect and a react component for it

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,60 @@
 import "./App.css";
 
-function App() {
+type HoverCard = {
+  bgImgSrc: string;
+  logoImgSrc: string;
+  fgImgSrc: string;
+};
+
+function HoverCardView({ bgImgSrc, logoImgSrc, fgImgSrc }: HoverCard) {
   return (
     <div className="card">
       <div className="pop-out">
-        <img
-          className="logo-image"
-          src="/src/assets/tyrion_logo_1.png"
-          alt="logo"
-        />
-        <img
-          className="front-image"
-          src="/src/assets/tyrion_fg_1.png"
-          alt="fg"
-        />
-        <img
-          className="bg-image"
-          src="https://github.com/ImCitizen13/3d-cover-hover/blob/main/src/assets/tyrion_bg_4.jpeg?raw=true"
-          alt="fg"
-        />
+        <img className="logo-image" src={logoImgSrc} alt="logo" />
+        <img className="front-image" src={fgImgSrc} alt="fg" />
+
+        <img className="bg-image" src={bgImgSrc} alt="fg" />
       </div>
+    </div>
+  );
+}
+
+function App() {
+  const hoverCards: HoverCard[] = [
+    {
+      bgImgSrc:
+        "https://github.com/ImCitizen13/3d-cover-hover/blob/main/src/assets/tyrion_bg.jpeg?raw=true",
+      logoImgSrc:
+        "https://github.com/ImCitizen13/3d-cover-hover/blob/main/src/assets/tyrion_logo.png?raw=true",
+      frontImgSrc:
+        "https://github.com/ImCitizen13/3d-cover-hover/blob/main/src/assets/tyrion_fg.png?raw=true",
+    },
+    {
+      bgImgSrc:
+        "https://github.com/ImCitizen13/3d-cover-hover/blob/main/src/assets/onepiece_bg.jpeg?raw=true",
+      logoImgSrc:
+        "https://github.com/ImCitizen13/3d-cover-hover/blob/main/src/assets/onepiece_logo.jpeg?raw=true",
+      frontImgSrc:
+        "https://github.com/ImCitizen13/3d-cover-hover/blob/main/src/assets/onepiece_fg.png?raw=true",
+    },
+    {
+      bgImgSrc:
+        "https://github.com/ImCitizen13/3d-cover-hover/blob/main/src/assets/tyrion_bg.jpeg?raw=true",
+      logoImgSrc:
+        "https://github.com/ImCitizen13/3d-cover-hover/blob/main/src/assets/tyrion_logo.png?raw=true",
+      frontImgSrc:
+        "https://github.com/ImCitizen13/3d-cover-hover/blob/main/src/assets/tyrion_fg.png?raw=true",
+    },
+  ];
+  return (
+    <div className="hover-cards">
+      {hoverCards.map((hCard) => {
+        return <HoverCardView
+          bgImgSrc={hCard.bgImgSrc}
+          logoImgSrc={hCard.logoImgSrc}
+          fgImgSrc={hCard.fgImgSrc}
+        ></HoverCardView>;
+      })}
     </div>
   );
 }
